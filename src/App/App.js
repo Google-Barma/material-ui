5645cf3ad48c0a92c578a29956d2ae4f0be85f02
Vadym <vad.evlanov@gmail.com>
2021-02-04 16:38:25 +0200

    add Header

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,31 @@
 import SideMenu from "../components/SideMenu";
 import "./App.css";
-import { CssBaseline, makeStyles } from "@material-ui/core";
+import {
+  createMuiTheme,
+  CssBaseline,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
 import Header from "../components/Header";
+import PageHeader from "../components/PageHeader";
+import PeopleOutlineTwoToneIcon from "@material-ui/icons/PeopleOutlineTwoTone";
+
+//смена основной политры цветов
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: "#3c44d126",
+    },
+    secondary: {
+      main: "#f83245",
+      light: "#f8324526",
+    },
+    background: {
+      default: "#f4f5fd",
+    },
+  },
+});
 
 const useStyles = makeStyles({
   appMain: {
@@ -14,13 +38,18 @@ function App() {
   const classes = useStyles();
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SideMenu />
       <div className={classes.appMain}>
         <Header />
+        <PageHeader
+          title="Page Header"
+          subtitle="Page description"
+          icon={<PeopleOutlineTwoToneIcon fontSize="large" />}
+        />
       </div>
       <CssBaseline />
-    </>
+    </ThemeProvider>
   );
 }
 
